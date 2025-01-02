c0a7d53d1e387a9e02da48c7e6d89faed57a4088
Víctor J. <maximovj@outlook.com>
2025-01-01 22:26:06 -0600

    * dev-progreso : Se agrega tarjeta de progreso en el dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 @php
     Widget::add()
+    ->to('before_content')
     ->type('div')
     ->class('row mb-2')
     ->content([
@@ -17,6 +18,48 @@
 
 @php
     Widget::add()
+    ->to('before_content')
+    ->type('div')
+    ->class('row mb-2')
+    ->content([
+        [
+            'type'        => 'progress',
+            'class'       => 'card text-white bg-type-2 mb-2',
+            'value'       => \App\Models\FilePDF::count(),
+            'description' => 'Archivos PDF',
+            'progress'    => 0, // integer
+            'hint'        => 'Archivos PDF de tipo formulario.',
+        ],
+        [
+            'type'        => 'progress',
+            'class'       => 'card text-white bg-type-4 mb-2',
+            'value'       => \App\Models\ViewForm::count()  ,
+            'description' => 'Formularios',
+            'progress'    => 0, // integer
+            'hint'        => 'Formularios vinculados con un archivo PDF',
+        ],
+        [
+            'type'        => 'progress',
+            'class'       => 'card text-white bg-type-5 mb-2',
+            'value'       => App\Models\GeneratePDF::countGenerated(),
+            'description' => 'Generado',
+            'progress'    => 0, // integer
+            'hint'        => 'Formulario generado en PDF',
+        ],
+        [
+            'type'        => 'progress',
+            'class'       => 'card txt-black bg-slate mb-2',
+            'value'       => App\Models\GeneratePDF::countDraft(),
+            'description' => 'Borrador',
+            'progress'    => 0, // integer
+            'hint'        => 'Formulario guardado como borrador.',
+        ],
+    ]);
+@endphp
+
+@php
+    Widget::add()
+    ->to('before_content')
     ->type('div')
     ->class('row mb-2')
     ->content([
